970f9bbaa7969aaad6489ca2f47174ee7b287eb5
Santiago Tena Hernandez <stenahe@gmail.com>
2022-09-28 00:58:56 +0200

    add parse_args connection

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:12:33 by stena-he          #+#    #+#             */
-/*   Updated: 2022/09/27 21:41:28 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/09/28 00:53:53 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,8 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack		a_stack;
-	
-	if ()
-	{
-		// print_help();
-		// return (0);
-	}
-	if ()
-		// init_mandelbrot(argv);
-	else if ()
-		// init_julia(argv);
-	else if ()
-		// print_help();
-	else
-	{
-		// ft_printf("Wait. That's illegal.\n\n");
-		// print_help();
-	}
+	if (argc == 1)
+		exit (0);
+	parse_args(argv);
 	return (0);
 }
