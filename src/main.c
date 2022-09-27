f12fe8ad4f07bce80bf62a0c92f7715a174ff4cb
stena-he <stena-he@student.42wolfsburg.de>
2022-09-27 20:26:24 +0200

    create main.c

---
+++ src/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 20:12:33 by stena-he          #+#    #+#             */
+/*   Updated: 2022/09/27 20:24:43 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	main(void)
+{
+	if ()
+	{
+		// print_help();
+		// return (0);
+	}
+	if ()
+		// init_mandelbrot(argv);
+	else if ()
+		// init_julia(argv);
+	else if ()
+		// print_help();
+	else
+	{
+		// ft_printf("Wait. That's illegal.\n\n");
+		// print_help();
+	}
+	return (0);
+}
