f96764521627c8a66d741481081daa0912007fc8
stena-he <stena-he@student.42wolfsburg.de>
2022-09-27 20:25:52 +0200

    create push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 20:13:41 by stena-he          #+#    #+#             */
+/*   Updated: 2022/09/27 20:23:38 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/* Parameters */
+
+
+/* Libraries */
+//Std libraries
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+//Custom libraries
+# include "libraries/ft_printf/libft/libft.h"
+# include "libraries/ft_printf/ft_printf.h"
+
+/* Structs */
+//Fractal parameters 
+// typedef struct s_fractol
+// {
+// 	void			*mlx;
+// 	void			*win;
+// 	char			*f_name;
+// 	double			min_r;
+// 	double			max_r;
+// 	double			min_i;
+// 	double			max_i;
+// 	double			kr;
+// 	double			ki;
+// 	struct s_img	*pimg;
+// }				t_fractol;
+
+/* Functions */
+// // Help
+// void	print_help(void);
+
+// // Draw fractals //
+// // Mandelbrot
+// void	init_mandelbrot(char **argv);
+// void	mandelbrot(t_img *img, t_point *p);
+// void	draw_mandelbrot(t_fractol *f, t_img *img);
+// // Julia
+// void	init_julia(char **argv);
+// void	julia(t_fractol *f, t_img *img, t_point *p);
+// void	draw_julia(t_fractol *f, t_img *img);
+
+#endif
