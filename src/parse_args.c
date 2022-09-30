09c2efd06ae20c6c62cb0fc3e0f1776c4560552d
Santiago Tena Hernandez <stenahe@gmail.com>
2022-09-30 14:17:59 +0200

    add is_sorted and check_dup functions

--- src/parse_args.c
+++ src/parse_args.c
@@ -6,48 +6,48 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 23:25:09 by stena-he          #+#    #+#             */
-/*   Updated: 2022/09/30 13:07:52 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/09/30 14:13:06 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-// void	is_sorted(t_stack *a_stack)
-// {
-// 	while (a_stack->next != NULL)
-// 	{
-// 		if (a_stack->value > a_stack->next->value)
-// 			break ;
-// 		a_stack = a_stack->next;
-// 	}
-// 	exit (0);
-// }
+int	is_sorted(t_stack *a_stack)
+{
+	while (a_stack->next != NULL)
+	{
+		if (a_stack->value > a_stack->next->value)
+			return (0) ;
+		a_stack = a_stack->next;
+	}
+	return (1);
+}
 
-// void	check_dup(char *argv)
-// {
-// 	int		counter;
-// 	char	test;
-// 	char	current;
+int	check_dup(char **argv)
+{
+	int		i;
+	int 	j;
+	int		counter;
 
-// 	counter = 0;
-// 	test = *argv;
-// 	current = *argv;
-// 	while (test)
-// 	{
-// 		while (current)
-// 		{
-// 			if ( ft_atoi_mod(argv) == ft_atoi_mod(argv))
-// 				counter++;
-// 			current++;
-// 		}
-// 		if (counter >= 2)
-// 			error_exit();
-// 		counter = 0;
-// 		test++;
-// 		current = *argv;
-// 	}
-// 	return ;
-// }
+	i = 1;
+	j = 1;
+	counter = 0;
+	while (argv[i])
+	{
+		while (argv[j])
+		{
+			if ( ft_atoi_mod(argv[i]) == ft_atoi_mod(argv[j]))
+				counter++;
+			j++;
+		}
+		if (counter >= 2)
+			return (1);
+		counter = 0;
+		i++;
+		j = 1;
+	}
+	return (0);
+}
 
 t_stack	*parse_args(char **argv)
 {
@@ -64,16 +64,12 @@ t_stack	*parse_args(char **argv)
 		new = ft_atoi_mod(argv[index]);
 		node = ft_lstnew_mod(new);
 		ft_lstadd_back_mod(&a_stack, node);
-		// free_stack(&node);
-		// printf("%d", a_stack->value);
 		index++;
 	}
+	if (check_dup(argv))
+		error_exit();
+	if (is_sorted(a_stack))
+		exit(0);
 	return (a_stack);
-	// free_stack(&a_stack);
-	// a_stack = head;
-	// printf("%d", a_stack->value);
-	
-	// check_dup(*argv);
-	// is_sorted(&a_stack);
 }
 
