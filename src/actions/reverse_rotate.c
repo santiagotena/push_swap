139a344865897180617d403640196d35f2fee754
stena-he <stena-he@student.42wolfsburg.de>
2022-10-03 00:55:20 +0200

    add function squeletons

--- src/actions/reverse_rotate.c
+++ src/actions/reverse_rotate.c
@@ -6,8 +6,23 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 21:30:50 by stena-he          #+#    #+#             */
-/*   Updated: 2022/09/27 21:31:21 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/03 00:54:09 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
+
+// void	rra(t_stack **a_stack, int rrr_call)
+// {
+	
+// }
+
+// void	rrb(t_stack **b_stack, int rrr_call)
+// {
+	
+// }
+
+// void	rrr(t_stack **a_stack, t_stack **b_stack)
+// {
+	
+// }
