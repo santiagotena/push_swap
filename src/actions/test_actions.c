6f036fa340713ee58c81338efb9618395b0416c1
stena-he <stena-he@student.42wolfsburg.de>
2022-10-02 23:52:14 +0200

    test ss

--- src/actions/test_actions.c
+++ src/actions/test_actions.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 12:20:40 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/02 23:35:27 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/02 23:51:09 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ void	test_actions(t_stack **a_stack, t_stack **b_stack)
 	// pb(a_stack, b_stack);
 	// print_stacks(a_stack, b_stack);
 
-	sa(a_stack);
+	sa(a_stack, 0);
 	print_stacks(a_stack, b_stack);
 	pb(a_stack, b_stack);
 	pb(a_stack, b_stack);
 	print_stacks(a_stack, b_stack);
-	sb(b_stack);
+	sb(b_stack, 0);
+	print_stacks(a_stack, b_stack);
+	ss(a_stack, b_stack);
 	print_stacks(a_stack, b_stack);
 }
 
