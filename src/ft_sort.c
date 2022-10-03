70668163dc8dcb65d85c7a812f09b0fb81a0335b
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-03 16:22:48 +0200

    able to compile

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -6,16 +6,16 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:45:12 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/03 15:50:14 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/03 16:13:42 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	sort_more(t_stack **a_stack, t_stack **b_stack)
-{
+// void	sort_more(t_stack **a_stack, t_stack **b_stack)
+// {
 	
-}
+// }
 
 void	sort_three(t_stack **a_stack)
 {
@@ -37,10 +37,11 @@ void	ft_sort(t_stack **a_stack, t_stack **b_stack)
 {
 	int		a_stack_len;
 
+	b_stack = NULL; //Delete
 	a_stack_len = ft_lst_len(a_stack);
 	if (a_stack_len == 2)
 		sa(a_stack, 0);
 	if (a_stack_len == 3)
 		sort_three(a_stack);
-	sort_more(a_stack, b_stack);
+	// sort_more(a_stack, b_stack);
 }
