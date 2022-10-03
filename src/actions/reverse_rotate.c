26705cb98d4238b0417c6d7ed217b384b481346a
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-03 12:23:26 +0200

    add descriptions

--- src/actions/reverse_rotate.c
+++ src/actions/reverse_rotate.c
@@ -6,23 +6,44 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 21:30:50 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/03 00:54:09 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/03 12:21:54 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-// void	rra(t_stack **a_stack, int rrr_call)
-// {
+/**
+ * @brief Shift down all elements of stack a by 1.
+The last element becomes the first one.
+ * 
+ * @param a_stack 
+ * @param rrr_call 
+ */
+void	rra(t_stack **a_stack, int rrr_call)
+{
 	
-// }
+}
 
-// void	rrb(t_stack **b_stack, int rrr_call)
-// {
-	
-// }
+/**
+ * @brief Shift down all elements of stack b by 1. 
+ * The last element becomes the first one.
 
-// void	rrr(t_stack **a_stack, t_stack **b_stack)
-// {
+ * 
+ * @param b_stack 
+ * @param rrr_call 
+ */
+void	rrb(t_stack **b_stack, int rrr_call)
+{
 	
-// }
+}
+
+/**
+ * @brief rra and rrb at the same time.
+ * 
+ * @param a_stack 
+ * @param b_stack 
+ */
+void	rrr(t_stack **a_stack, t_stack **b_stack)
+{
+	
+}
--- src/actions/rotate.c
+++ src/actions/rotate.c
@@ -6,23 +6,43 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 21:30:34 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/03 00:54:14 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/03 12:20:57 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-// void	ra(t_stack **a_stack, int rr_call)
-// {
+/**
+ * @brief Shift up all elements of stack a by 1.
+The first element becomes the last one.
+ * 
+ * @param a_stack 
+ * @param rr_call 
+ */
+void	ra(t_stack **a_stack, int rr_call)
+{
 	
-// }
+}
 
-// void	rb(t_stack **b_stack, int rr_call)
-// {
+/**
+ * @brief Shift up all elements of stack b by 1. 
+ * The first element becomes the last one.
+ * 
+ * @param b_stack 
+ * @param rr_call 
+ */
+void	rb(t_stack **b_stack, int rr_call)
+{
 	
-// }
+}
 
-// void	rr(t_stack **a_stack, t_stack **b_stack)
-// {
+/**
+ * @brief ra and rb at the same time.
+ * 
+ * @param a_stack 
+ * @param b_stack 
+ */
+void	rr(t_stack **a_stack, t_stack **b_stack)
+{
 	
-// }
+}
