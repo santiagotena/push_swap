1779cfbaea5efa7ddd522f3f57bf1fcfe9a22309
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-05 19:18:06 +0200

    update add_pos

--- src/pass_to_a.c
+++ src/pass_to_a.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:26:28 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/05 17:38:52 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/05 19:17:54 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,10 @@ void	find_target_pos(t_stack **a_stack, t_stack **b_stack)
 	t_stack	*node_b;
 	int		max_a_index;
 	int		min_a_index;
-	int		start_tp;
-	int		is_change;
 	
 	node_a = *a_stack;
 	node_b = *b_stack;
 	max_a_index = 0;
-	is_change = 1;
 	while (node_a)
 	{
 		if (max_a_index < node_a->index)
@@ -56,7 +53,6 @@ void	find_target_pos(t_stack **a_stack, t_stack **b_stack)
 		node_a = node_a->next;
 	}
 	node_a = *a_stack;
-
 	min_a_index = max_a_index;
 	while (node_a)
 	{
@@ -66,33 +62,8 @@ void	find_target_pos(t_stack **a_stack, t_stack **b_stack)
 	}
 	node_a = *a_stack;
 
-	// printf("\n\n%d %d\n\n", min_a_index, max_a_index);
 
-	while (is_change)
-	{
-		is_change = 0;
-		while (node_b)
-		{
-			start_tp = node_b->target_pos;
-			if (node_b->index < max_a_index)
-			{
-				while (node_a)
-				{
-					if (node_b->index == node_a->index - 1)
-						node_b->target_pos = node_a->pos;
-					node_a = node_a->next;
-				}
-				node_a = *a_stack;
-			}
-			else
-				node_b->target_pos = min_a_index;
-			if (start_tp != node_b->target_pos)
-				is_change = 1;
-			node_b = node_b->next;
-		}
-		node_b = *b_stack;
-	}
-	// print_stacks(a_stack, b_stack);
+
 }
 
 void	pass_to_a(t_stack **a_stack, t_stack **b_stack)
