1a848aa1c0c73d9cd236341cdaae08bb0cf26d3e
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-08 14:39:18 +0200

    wc -l test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:12:33 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/08 04:20:57 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/08 14:11:05 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,16 @@ int	main(int argc, char **argv)
 		return (0);
 	a_stack = parse_args(argv);
 
-	ft_printf("Start:\n");//Delete
-	print_stacks(&a_stack, &b_stack);
+	// ft_printf("Start:\n");//Delete
+	// print_stacks(&a_stack, &b_stack);
+	
 	// ft_printf("Actions:\n");
 	// test_actions(&a_stack, &b_stack);//Delete //
 
 	ft_sort(&a_stack, &b_stack);
 
-	ft_printf("\nEnd:\n");//Delete
-	print_stacks(&a_stack, &b_stack);//Delete //
+	// ft_printf("\nEnd:\n");//Delete
+	// print_stacks(&a_stack, &b_stack);//Delete //
 
 	free_stack(&a_stack);
 	free_stack(&b_stack);
