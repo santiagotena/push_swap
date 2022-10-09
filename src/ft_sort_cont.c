31e7f01ec62aab472ea0e1ebf50e9f67d06ced35
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-10 01:28:56 +0200

    improve until exec_act, sorts 500+ numbers

--- src/ft_sort_cont.c
+++ src/ft_sort_cont.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:26:28 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/09 21:09:49 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/10 01:23:16 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,50 +49,64 @@ void	final_sort(t_stack **a_stack)
 void	exec_act(t_stack **a_stack, t_stack **b_stack)
 {
 	t_stack	*node_b;
-	t_stack	*lowest_node;
 	int		lowest_cost;
 	int		cost;
+	int		cost_a;
+	int		cost_b;
 	
 	node_b = *b_stack;
-	lowest_node = *b_stack;
 	lowest_cost = abs_val(node_b->cost_a) + abs_val(node_b->cost_b);
-	if (node_b->next == NULL)
-		return ;
 	while (node_b)
 	{
 		cost = abs_val(node_b->cost_a) + abs_val(node_b->cost_b);
-		if (cost < lowest_cost)
+		if (cost <= lowest_cost)
 		{
+			cost_a = node_b->cost_a;
+			cost_b = node_b->cost_b;
 			lowest_cost = cost;
-			lowest_node = node_b;
 		}
 		node_b = node_b->next;
 	}
-	while (lowest_node->cost_a != 0 || lowest_node->cost_b != 0)
+	if (cost_a != 0 || cost_b != 0)
 	{
-		if (lowest_node->cost_a > 0 && lowest_node->cost_b > 0)
-			rr(a_stack, b_stack);
-		else if (lowest_node->cost_a < 0 && lowest_node->cost_b < 0)
-			rrr(a_stack, b_stack);
-		else
+		while (cost_a > 0 && cost_b > 0)
 		{
-			if (lowest_node->cost_a > 0)
-				ra(a_stack, 0);
-			if (lowest_node->cost_b > 0)
-				rb(b_stack, 0);
-			if (lowest_node->cost_a < 0)
-				rra(a_stack, 0);
-			if (lowest_node->cost_b < 0)
-				rrb(b_stack, 0);
+			rr(a_stack, b_stack);
+			cost_a--;
+			cost_b--;
 		}
-			if (lowest_node->cost_a > 0)
-				lowest_node->cost_a--;
-			if (lowest_node->cost_b > 0)
-				lowest_node->cost_b--;
-			if (lowest_node->cost_a < 0)
-				lowest_node->cost_a++;
-			if (lowest_node->cost_b < 0)
-				lowest_node->cost_b++;
+		while (cost_a < 0 && cost_b < 0)
+		{
+			rrr(a_stack, b_stack);
+			cost_a++;
+			cost_b++;
+		}
+	}
+	if (cost_a != 0)
+	{
+		while (cost_a > 0)
+		{
+			ra(a_stack, 0);
+			cost_a--;
+		}
+		while (cost_a < 0)
+		{
+			rra(a_stack, 0);
+			cost_a++;
+		}
+	}
+	if (cost_b != 0)
+	{
+		while (cost_b > 0)
+		{
+			rb(b_stack, 0);
+			cost_b--;
+		}
+		while (cost_b < 0)
+		{
+			rrb(b_stack, 0);
+			cost_b++;
+		}
 	}
 }
 
@@ -119,51 +133,38 @@ void	calc_cost(t_stack **a_stack, t_stack **b_stack)
 	}
 }
 
-// Need to improve
-void	find_tp_cont(t_stack **a_stack, t_stack *node_b, int max_a_index)
-{
-	t_stack	*node_a;
-	int		lowest_diff;
-	int		diff;
-
-	node_a = *a_stack;
-	lowest_diff = max_a_index - 1;
-	while (node_a)
-	{
-		diff = node_a->index - node_b->index;
-		if (diff > 0 && diff < lowest_diff)
-		{
-			lowest_diff = diff;
-			node_b->target_pos = node_a->pos;
-		}
-		node_a = node_a->next;
-	}
-}
-
 void	find_tp(t_stack **a_stack, t_stack **b_stack)
 {
 	t_stack	*node_a;
 	t_stack	*node_b;
-	t_stack	*low_ind_a_node;
 	int		max_a_index;
+	int		low_a_ind_pos;
+	int		lowest_diff;
+	int		diff;
 	
 	node_a = *a_stack;
-	low_ind_a_node = *a_stack;
 	node_b = *b_stack;
 	max_a_index = get_maxind(a_stack);
-	while (node_a)
-	{
-		if (node_a->index < low_ind_a_node->index)
-			low_ind_a_node = node_a;
-		node_a = node_a->next;
-	}
+	low_a_ind_pos = get_lowind_pos(a_stack);
+	lowest_diff = max_a_index;
 	while (node_b)
 	{
 		if (node_b->index > max_a_index)
-			node_b->target_pos = low_ind_a_node->pos;
+			node_b->target_pos = low_a_ind_pos;
 		else
 		{
-			find_tp_cont(a_stack, node_b, max_a_index);
+			lowest_diff = max_a_index;
+			while (node_a)
+			{
+				diff = node_a->index - node_b->index;
+				if (diff > 0 && diff < lowest_diff)
+				{
+					lowest_diff = diff;
+					node_b->target_pos = node_a->pos;
+				}
+				node_a = node_a->next;
+			}
+			node_a = *a_stack;
 		}
 		node_b = node_b->next;
 	}
