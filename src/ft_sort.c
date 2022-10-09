6183a9e57432a433dde63f549003c2073960d4c8
Santiago Tena Hernandez <stenahe@gmail.com>
2022-10-10 00:03:47 +0200

    add easier testing

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:45:12 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/09 21:33:55 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/09 23:59:23 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,21 +55,29 @@ void	sort_more(t_stack **a_stack, t_stack **b_stack)
 {
 	pass_to_b(a_stack, b_stack);
 	sort_three(a_stack);
-	while (*b_stack)
+	// while (*b_stack)
+	// {
+	// 	add_pos(a_stack, b_stack);
+	// 	find_tp(a_stack, b_stack);
+	// 	calc_cost(a_stack, b_stack);
+	// 	exec_act(a_stack, b_stack);
+	// 	pa(a_stack, b_stack);
+	// }
+	final_sort(a_stack);
+
+	int counter = 2;
+	while (counter)
 	{
 		add_pos(a_stack, b_stack);
 		find_tp(a_stack, b_stack);
-		calc_cost(a_stack, b_stack);
-		exec_act(a_stack, b_stack);
+		print_stacks(a_stack, b_stack);//Delete //
+		// calc_cost(a_stack, b_stack);
+		// exec_act(a_stack, b_stack);
 		pa(a_stack, b_stack);
+		// print_stacks(a_stack, b_stack);//Delete //
+		
+		counter --;
 	}
-	final_sort(a_stack);
-
-	// add_pos(a_stack, b_stack);
-	// find_tp(a_stack, b_stack);
-	// calc_cost(a_stack, b_stack);
-	// exec_act(a_stack, b_stack);
-	// pa(a_stack, b_stack);
 }
 
 void	ft_sort(t_stack **a_stack, t_stack **b_stack)
