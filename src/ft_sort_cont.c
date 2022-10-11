63ac9a5e363bef0a6d14fc06b43ada5908c9607d
stena-he <stena-he@student.42wolfsburg.de>
2022-10-11 23:32:31 +0200

    norminette

--- src/ft_sort_cont.c
+++ src/ft_sort_cont.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:26:28 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/11 22:46:49 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/11 23:31:29 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,38 +35,43 @@ void	calc_cost(t_stack **a_stack, t_stack **b_stack)
 	}
 }
 
-void	find_tp(t_stack **a_stack, t_stack **b_stack)
+void	find_tp_lowind(t_stack **a_stack, t_stack *node_b)
 {
 	t_stack	*node_a;
-	t_stack	*node_b;
-	int		max_a_index;
-	int		low_a_ind_pos;
 	int		lowest_diff;
 	int		diff;
 
 	node_a = *a_stack;
+	lowest_diff = get_maxind(a_stack);
+	while (node_a)
+	{
+		diff = node_a->index - node_b->index;
+		if (diff > 0 && diff < lowest_diff)
+		{
+			lowest_diff = diff;
+			node_b->target_pos = node_a->pos;
+		}
+		node_a = node_a->next;
+	}
+	node_a = *a_stack;
+}
+
+void	find_tp(t_stack **a_stack, t_stack **b_stack)
+{
+	t_stack	*node_b;
+	int		max_a_index;
+	int		low_a_ind_pos;
+
 	node_b = *b_stack;
 	max_a_index = get_maxind(a_stack);
 	low_a_ind_pos = get_lowind_pos(a_stack);
-	lowest_diff = max_a_index - 1;
 	while (node_b)
 	{
 		if (node_b->index > max_a_index)
 			node_b->target_pos = low_a_ind_pos;
 		else
 		{
-			lowest_diff = max_a_index;
-			while (node_a)
-			{
-				diff = node_a->index - node_b->index;
-				if (diff > 0 && diff < lowest_diff)
-				{
-					lowest_diff = diff;
-					node_b->target_pos = node_a->pos;
-				}
-				node_a = node_a->next;
-			}
-			node_a = *a_stack;
+			find_tp_lowind(a_stack, node_b);
 		}
 		node_b = node_b->next;
 	}
