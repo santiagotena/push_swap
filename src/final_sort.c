832d3899f981de511a6b423de6872d9639a3f722
stena-he <stena-he@student.42wolfsburg.de>
2022-10-12 00:13:12 +0200

    norminette

--- src/final_sort.c
+++ src/final_sort.c
@@ -6,28 +6,17 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 22:47:01 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/11 22:47:39 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/12 00:12:54 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	final_sort(t_stack **a_stack)
+void	do_final_actions(t_stack **a_stack, int lowest_pos)
 {
-	t_stack	*node_a;
-	int		lowest_pos;
 	int		stack_size;
 
-	node_a = *a_stack;
 	stack_size = ft_lst_len(a_stack);
-	lowest_pos = 0;
-	while (node_a)
-	{
-		if (node_a->index == 1)
-			break ;
-		lowest_pos++;
-		node_a = node_a->next;
-	}
 	if (lowest_pos > stack_size / 2)
 	{
 		while (lowest_pos < stack_size)
@@ -44,4 +33,21 @@ void	final_sort(t_stack **a_stack)
 			lowest_pos--;
 		}
 	}
+}
+
+void	final_sort(t_stack **a_stack)
+{
+	t_stack	*node_a;
+	int		lowest_pos;
+
+	node_a = *a_stack;
+	lowest_pos = 0;
+	while (node_a)
+	{
+		if (node_a->index == 1)
+			break ;
+		lowest_pos++;
+		node_a = node_a->next;
+	}
+	do_final_actions(a_stack, lowest_pos);
 }
