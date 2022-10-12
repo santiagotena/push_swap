1baeb19f62003d707bcb5f128e6672f352aa7cd9
stena-he <stena-he@student.42wolfsburg.de>
2022-10-12 02:36:16 +0200

    restructure file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:13:41 by stena-he          #+#    #+#             */
-/*   Updated: 2022/10/11 22:43:34 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/10/12 02:33:38 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,30 @@ typedef struct s_stack
 }					t_stack;
 
 /* Functions */
+// Parse arguments //
+t_stack		*parse_args(char **argv);
+int			is_sorted(t_stack *a_stack);
+// Sort //
+void		ft_sort(t_stack **a_stack, t_stack **b_stack);
+void		add_pos(t_stack **a_stack, t_stack **b_stack);
+void		find_tp(t_stack **a_stack, t_stack **b_stack);
+void		calc_cost(t_stack **a_stack, t_stack **b_stack);
+void		exec_actions(t_stack **a_stack, t_stack **b_stack);
+void		final_sort(t_stack **a_stack);
+// Sort utils
+int			get_maxind(t_stack **stack);
+int			get_lowind_pos(t_stack **stack);
+// Linked lists //
+int			ft_lst_len(t_stack **lst);
+t_stack		*ft_lstnew_mod(int value, int index);
+t_stack		*add_lsvalues(t_stack **stack);
+void		ft_lstadd_back_mod(t_stack **lst, t_stack *new);
+void		ft_lstadd_front_mod(t_stack **lst, t_stack *new);
+void		free_stack(t_stack **stack);
+// Utils //
+void		error_exit(void);
+int			abs_val(int nb);
+int			ft_atoi_mod(const char *str);
 // Actions //
 //Push
 void		pa(t_stack **a_stack, t_stack **b_stack);
@@ -53,39 +77,5 @@ void		rr(t_stack **a_stack, t_stack **b_stack);
 void		rra(t_stack **a_stack, int rrr_call);
 void		rrb(t_stack **b_stack, int rrr_call);
 void		rrr(t_stack **a_stack, t_stack **b_stack);
-
-// Test actions //
-void		test_actions(t_stack **a_stack, t_stack **b_stack); //Delete
-void		read_stack(t_stack **stack);
-void		print_stacks(t_stack **a_stack, t_stack **b_stack);//Delete//
-
-// Sort //
-void		ft_sort(t_stack **a_stack, t_stack **b_stack);
-void		add_pos(t_stack **a_stack, t_stack **b_stack);
-void		find_tp(t_stack **a_stack, t_stack **b_stack);
-void		calc_cost(t_stack **a_stack, t_stack **b_stack);
-void		exec_actions(t_stack **a_stack, t_stack **b_stack);
-void		final_sort(t_stack **a_stack);
-
-// Sort utils
-int			get_maxind(t_stack **stack);
-int			get_lowind_pos(t_stack **stack);
-
-// Parse arguments //
-t_stack		*parse_args(char **argv);
-int			is_sorted(t_stack *a_stack);
-
-// Linked lists //
-int			ft_lst_len(t_stack **lst);
-t_stack		*ft_lstnew_mod(int value, int index);
-t_stack		*add_lsvalues(t_stack **stack);
-void		ft_lstadd_back_mod(t_stack **lst, t_stack *new);
-void		ft_lstadd_front_mod(t_stack **lst, t_stack *new);
-void		free_stack(t_stack **stack);
-
-// Utils //
-void		error_exit(void);
-int			abs_val(int nb);
-int			ft_atoi_mod(const char *str);
 
 #endif
